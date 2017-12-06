448ba45f6afa8de560b3270e92483f3f0acacf61
noyouaretheman <noyouaretheman@gmail.com>
2017-12-05 20:42:17 -0700

    periodic update

--- .config/menutray/config.pl
+++ .config/menutray/config.pl
@@ -20,20 +20,13 @@
     | substitutions       : Substitute, by using a regex, in the values of the desktop files.
                             Example: [
                                 {key => 'Exec', re => qr/xterm/, value => 'sakura'},
-                                {key => 'Exec', re => qr/\\\\/,  value => '\\', global => 1},    # for wine apps
+                                {key => 'Name', re => qr/^GNU I\w+ M\w+ P\w+/, value => 'GIMP'},
                             ],
 
 || ICON SETTINGS
     | icon_type           : Menu icon type (menu, dnd, small-toolbar, large-toolbar, button, dialog)
     | icon_size           : Icon size in pixels (only for absolute icon paths) (default: [16, 16])
     | missing_image       : Use this icon for missing icons (default: gtk-missing-image)
-
-|| KEYS
-    | tooltip_keys        : Valid keys for the tooltip text.
-                            Example: ['Comment[es]', 'Comment'],
-
-    | name_keys           : Valid keys for the item names.
-                            Example: ['Name[fr]', 'GenericName[fr]', 'Name'],   # french menu
 
 || PATHS
     | desktop_files_paths   : Absolute paths which contain .desktop files.
@@ -46,25 +39,24 @@
 =cut
 
 our $CONFIG = {
-  "editor"                 => "geany",
-  "gdk_interpolation_type" => "hyper",
-  "icon_size"              => [16, 16],
-  "icon_type"              => "menu",
-  "Linux::DesktopFiles"    => {
-                                desktop_files_paths     => ["/usr/share/applications", "/usr/local/share/applications"],
-                                keep_unknown_categories => 1,
-                                skip_entry              => undef,
-                                skip_filename_re        => undef,
-                                substitutions           => undef,
-                                terminal                => "urxvt",
-                                terminalization_format  => "%s -e '%s'",
-                                terminalize             => 1,
-                                unknown_category_key    => "other",
-                              },
-  "menutray_icon"          => "start-here-archlinux",
-  "missing_image"          => "gtk-missing-image",
-  "name_keys"              => ["Name"],
-  "set_tooltips"           => 1,
-  "tooltip_keys"           => ["Comment"],
-  "VERSION"                => "0.50",
+  "editor"              => "geany",
+  "icon_size"           => [16, 16],
+  "icon_type"           => "menu",
+  "Linux::DesktopFiles" => {
+                             desktop_files_paths     => ["/usr/share/applications", "/usr/local/share/applications"],
+                             keep_unknown_categories => 1,
+                             skip_entry              => undef,
+                             skip_filename_re        => undef,
+                             substitutions           => undef,
+                             terminal                => "urxvt",
+                             terminalization_format  => "%s -e '%s'",
+                             terminalize             => 1,
+                             unknown_category_key    => "other",
+                           },
+  "menutray_icon"       => "start-here-archlinux",
+  "menutray_title"      => "menutray",
+  "menutray_tooltip"    => "Applications",
+  "missing_image"       => "gtk-missing-image",
+  "tooltips"            => 1,
+  "VERSION"             => 0.51,
 }
