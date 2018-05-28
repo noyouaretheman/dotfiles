248cefb61c45008d2b01b5186a45f207cf265689
noyouaretheman <noyouaretheman@gmail.com>
2018-05-28 08:23:37 -0600

    added aliases for dsp builder

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -12,3 +12,9 @@ alias quartus64='quartus --64bit'
 
 # aliases for matlab
 alias matlab='matlab -desktop'
+
+# dsp builder
+alias dspb='$QUARTUS_ROOTDIR_OVERRIDE/dsp_builder/dsp_builder.sh &'
+
+# dsp builder advanced blockset
+alias dspba='$QUARTUS_ROOTDIR_OVERRIDE/dspba/dsp_builder.sh &'
